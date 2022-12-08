2de1bb45c45a866e791cb2eede820c657d418821
MichalSmyk <michal.smyk39@gmail.com>
2022-12-08 13:46:09 +0000

    add another test

--- spec/diary-entry_spec.rb
+++ spec/diary-entry_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe DiaryEntry do
         expect(diary_entry.title).to eq "the_title"
         expect(diary_entry.contents).to eq "the_contents"
     end
-    describe "#count_words"
+    describe "#count_words" do 
+        it 'returns the number of words' do
+            diary_entry = DiaryEntry.new('my_title', "here are few words that need to be counted")
+            expect(diary_entry.count_words).to eq 9
+        end
+    end
 end
